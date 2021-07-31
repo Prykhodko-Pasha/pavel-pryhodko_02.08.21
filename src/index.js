4835e2c43cdc439fd0970e26b722302402febbe0
Prykhodko-Pasha <prykhodko.p@gmail.com>
2021-07-31 16:05:04 +0300

    adding to fav list

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const refs = {
   // themeCheckbox: document.querySelector('#theme-switch-toggle'),
   // body: document.querySelector('body'),
   movGall: document.querySelector('.mov-gall__list'),
+  favList: document.querySelector('.fav-list'),
   // switchMarker: document.querySelector('.theme-switch__marker'),
 };
 
@@ -44,6 +45,28 @@ const refs = {
 
 // создание и рендер разметки
 const movGallMarkup = movCards.map(card => movCardTpl(card)).join('');
-// console.log(galleryMarkup);
 
 refs.movGall.innerHTML = movGallMarkup;
+
+// добавление в список фаворитов
+refs.movGall.addEventListener('click', onAddToFavList);
+
+function onAddToFavList(e) {
+  // refs.switchMarker.style.transition = '';
+  // console.log(e.target.className);
+  if (e.target.className === 'chBox') {
+    if (e.target.checked) {
+      const movieId = parseInt(e.target.id) - 1;
+      // console.log(movieId);
+      const movieName = `<li class='fav-${e.target.id}'>${movCards[movieId].name}</li>`;
+      // console.log(movieName);
+      refs.favList.insertAdjacentHTML('beforeend', movieName);
+    } else {
+      const favItemIdx = `.fav-${e.target.id}`;
+      // console.log('favItemIdx: ', favItemIdx);
+      const favItemToDelete = document.querySelector(favItemIdx);
+      // console.log('favItemToDelete: ', favItemToDelete);
+      refs.favList.removeChild(favItemToDelete);
+    }
+  }
+}
