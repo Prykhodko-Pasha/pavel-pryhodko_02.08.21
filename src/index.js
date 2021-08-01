6ac89e76a43821f3f4dbd94f9a289678ec3858f3
Prykhodko-Pasha <prykhodko.p@gmail.com>
2021-08-01 19:52:33 +0300

    beginning ls

--- src/index.js
+++ src/index.js
@@ -3,45 +3,9 @@ import movCardTpl from './templates/mov-card.hbs';
 import movCards from './movies.json';
 
 const refs = {
-  // themeCheckbox: document.querySelector('#theme-switch-toggle'),
-  // body: document.querySelector('body'),
   movGall: document.querySelector('.mov-gall__list'),
   favList: document.querySelector('.fav-list'),
-  // switchMarker: document.querySelector('.theme-switch__marker'),
 };
-
-// const Theme = {
-//   LIGHT: 'light-theme',
-//   DARK: 'dark-theme',
-// };
-
-// установка темы, если до этого она была изменена
-// populateTheme();
-
-// function populateTheme() {
-//   const localStorageTheme = localStorage.getItem('theme');
-//   if (localStorageTheme === Theme.DARK) {
-//     refs.body.className = Theme.DARK;
-//     refs.themeCheckbox.checked = true;
-//     refs.switchMarker.style.transition = 'none';
-//   } else {
-//     refs.body.className = Theme.LIGHT;
-//   }
-// }
-
-// // изменение темы
-// refs.themeCheckbox.addEventListener('change', onChangeTheme);
-
-// function onChangeTheme() {
-//   refs.switchMarker.style.transition = '';
-//   if (refs.body.className === Theme.DARK) {
-//     refs.body.className = Theme.LIGHT;
-//     localStorage.setItem('theme', Theme.LIGHT);
-//   } else {
-//     refs.body.className = Theme.DARK;
-//     localStorage.setItem('theme', Theme.DARK);
-//   }
-// }
 
 // создание и рендер разметки
 const movGallMarkup = movCards.map(card => movCardTpl(card)).join('');
@@ -49,16 +13,15 @@ const movGallMarkup = movCards.map(card => movCardTpl(card)).join('');
 refs.movGall.innerHTML = movGallMarkup;
 
 // добавление в список фаворитов
-refs.movGall.addEventListener('click', onAddToFavList);
+refs.movGall.addEventListener('click', onStarClick);
 
-function onAddToFavList(e) {
-  // refs.switchMarker.style.transition = '';
-  // console.log(e.target.className);
+function onStarClick(e) {
   if (e.target.className === 'chBox') {
     if (e.target.checked) {
+      addStarIdToLocalSt(e.target.id);
       const movieId = parseInt(e.target.id) - 1;
       // console.log(movieId);
-      const movieName = `<li class='fav-${e.target.id}'>${movCards[movieId].name}</li>`;
+      const movieName = `<li class='fav-item fav-${e.target.id}'>${movCards[movieId].name}<button type="button" class="close-btn"></li>`;
       // console.log(movieName);
       refs.favList.insertAdjacentHTML('beforeend', movieName);
     } else {
@@ -68,5 +31,47 @@ function onAddToFavList(e) {
       // console.log('favItemToDelete: ', favItemToDelete);
       refs.favList.removeChild(favItemToDelete);
     }
+  }
+}
+
+// удаление со списка фаворитов нажатием на крестик
+refs.favList.addEventListener('click', e => {
+  // console.dir(e.target);
+  if (e.target.className === 'close-btn') {
+    onDelFromFavList(e);
+  }
+});
+
+function onDelFromFavList(e) {
+  // console.log(e.target.parentNode.textContent);
+  const movieIdx = e.target.parentNode.className.split('').pop();
+  // console.log(movieIdx);
+  const starToUncheck = document.getElementById(movieIdx);
+  // console.log(starToUncheck);
+  starToUncheck.checked = false;
+  e.target.parentNode.remove();
+}
+
+// запоминание списка фаворитов
+function addStarIdToLocalSt(id) {
+  if (localStorage.getItem('starsIdList')) {
+    const starsArr = JSON.parse(localStorage.getItem('starsIdList'));
+    starsArr.push(id);
+    localStorage.setItem('starsIdList', JSON.stringify(starsArr));
+  } else {
+    const starsArr = [id];
+    localStorage.setItem('starsIdList', JSON.stringify(starsArr));
+  }
+}
+
+onPopulateFavStars();
+function onPopulateFavStars() {
+  if (localStorage.getItem('starsIdList')) {
+    const starsArr = JSON.parse(localStorage.getItem('starsIdList'));
+    console.log(starsArr);
+    starsArr.map(id => {
+      const starToCheck = document.getElementById(id);
+      starToCheck.checked = true;
+    });
   }
 }
